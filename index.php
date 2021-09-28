5a1a09fdb239c8a598c12bef2fc4fc3f1e70ae3a
ThaoSe141101 <thaonttse141101@fpt.edu.vn>
2021-09-29 02:26:34 +0700

    upload index.php

--- index.php
+++ index.php
@@ -1,12 +1,1 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PRJ301 demo</title>
-</head>
-<body>
-    <h1>Welcom to PRJ</h1>
-</body>
-</html>
+<?php header( 'Location: /login.html') ; ?>
